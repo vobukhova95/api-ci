b661be3d0effa37babdd3379924483c89de0657e
Veronika Obukhova <vera.obukhova.95@gmail.com>
2025-06-05 23:59:38 +0700

    Passed SchemaTest

---
+++ src/test/java/ru/netology/rest/SchemaTest.java
@@ -0,0 +1,23 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class SchemaTest {
+    @Test
+    void shouldMatchSchemaTest() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+
+                .when()
+                .get("/demo/accounts")
+
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+
+
+    }
+}
